2571e24957e38cee6070b6a3e010cfac70ee7704
Matej Šlachta <matej.slachta366@gmail.com>
2023-09-10 02:36:11 +0200

    change directory

--- START.bat
+++ START.bat
@@ -3,6 +3,7 @@ color B
 :loop
 echo This script is was made by Matej Slachta (Corrupted/CheeseballSVK)
 echo ------------------------------------------------------------------
+cd program
 main.py
 timeout 5
 CLS
